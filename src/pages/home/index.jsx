5bb6c5135968568d04205189b79b762711c8058f
OnlyTrable <wrangler2005@gmail.com>
2025-07-07 10:09:10 +0200

    project setup

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,11 @@
+import  unionIcon  from "../../assets/icons/Union.svg"
+function Home () {
+    return (
+    <>    
+    <h1>Home</h1>
+    <img src={unionIcon} alt="unionIcon" />
+    </>
+    )
+}
+
+export default Home;
