6a91f8b04c1b49b1fa5749344f8126ec06996cec
OnlyTrable <wrangler2005@gmail.com>
2025-07-13 12:36:15 +0200

    розібрався з кривою у бекграунді

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
+import MainFrame from './components/layout/MainFrame'
 import Header from './components/layout/Header'
 import Main from './components/layout/main'
 import Footer from './components/layout/footer'
-import MainFrame from './components/layout/MainFrame'
 import './App.css'
 
 function App() {
   return (
-    <MainFrame>
-      <Header />
-      <Main />
-      <Footer />
-    </MainFrame>
+    <>
+      <div className="fullAppHeaderContainer">
+        <MainFrame>
+          <Header />
+        </MainFrame>
+      </div>
+      <MainFrame>
+        <Main />
+        <Footer />
+      </MainFrame>
+    </>
   )
 }
 
