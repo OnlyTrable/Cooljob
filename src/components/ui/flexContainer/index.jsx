99fc79b6d0b14e665b5f53f18409b4326799b58c
OnlyTrable <wrangler2005@gmail.com>
2025-07-17 01:31:48 +0200

    Нарешті правильно зробив svg

--- src/components/ui/flexContainer/index.jsx
+++ src/components/ui/flexContainer/index.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 
-// FlexContainer - це простий компонент-обгортка.
-// Він повинен приймати `children` як проп і рендерити їх.
-// Ми також передаємо решту пропсів (`...props`), щоб можна було задавати `className`, `id` і т.д.
 const FlexContainer = ({ children, ...props }) => {
   return (
     <div {...props}>{children}</div>
